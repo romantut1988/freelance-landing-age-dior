f3b94e5acbaddc08dfc5c494e98fa19df5ef487e
Roman <romantut1988@mail.ru>
2024-05-12 00:01:44 +0300

    slider top desktop

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    effect: 'fade',
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
