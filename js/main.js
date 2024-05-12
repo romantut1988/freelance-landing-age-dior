723ed066e66d3e6cb4d64aa603785d300c617e09
Roman <romantut1988@mail.ru>
2024-05-13 00:52:18 +0300

    slider about desktop

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiperTop = new Swiper('.top__swiper', {
     effect: 'fade',
     autoplay: {
         delay: 3500,
@@ -9,3 +9,14 @@ const swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+const swiperAbout = new Swiper(".about__slider", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    freeMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
